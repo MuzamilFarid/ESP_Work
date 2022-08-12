315daca282fa4541a94aa0a69b0fb3e36483e29e
MuzamilFarid <muzamilf94@gmail.com>
2022-08-12 19:32:26 +0200

    some minor change

--- Esp8266_Webserver/src/main.cpp
+++ Esp8266_Webserver/src/main.cpp
@@ -2,8 +2,12 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(D5,OUTPUT);
+  digitalWrite(D5,HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
+
 }
